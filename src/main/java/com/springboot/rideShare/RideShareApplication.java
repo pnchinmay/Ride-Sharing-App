89b7b598ec64bba54e28593e2985f046a85e36f6
pnchinmay <pnchinmay9@gmail.com>
2023-04-12 00:16:18 +0530

    Initial commit

---
+++ src/main/java/com/springboot/rideShare/RideShareApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.rideShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideShareApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/springboot/rideShare/RideShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.rideShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
